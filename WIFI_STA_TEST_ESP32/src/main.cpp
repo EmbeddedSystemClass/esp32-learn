de41893e59e3ec97749f50f5669993db6c248963
Koson Trachu <koson.trachu@gmail.com>
2020-02-08 23:11:46 +0700

    add wifi sta test

---
+++ WIFI_STA_TEST_ESP32/src/main.cpp
@@ -0,0 +1,31 @@
+#include<WiFi.h>
+
+#define WIFI_STA_NAME "AESFIBER"
+#define WIFI_STA_PASS "     "
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_STA_NAME);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_STA_NAME, WIFI_STA_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() { }
